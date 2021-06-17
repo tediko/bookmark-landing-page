1d3eedcb0e8f0500d19f5756e54808b2432c7fa8
tediko <tediko123@wp.pl>
2021-06-17 16:33:16 +0200

    Add: Header section

---
+++ src/js/components/Header.js
@@ -0,0 +1,18 @@
+import MobileNav from './MobileNav';
+import DesktopNav from './DesktopNav';
+import logo from '../../assets/img/logo-bookmark.svg';
+
+const Header = () => {
+    return ( 
+        <header className="header">
+            <div className="header__container">
+                <a href="#" className="header__logo">
+                    <img src={logo} alt="Bookmark - Home page" className="header__logo-img" />
+                </a>
+                <MobileNav />
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
