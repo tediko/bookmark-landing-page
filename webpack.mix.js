6994ae8831e624f0be324fe3429b1fc8f209d61e
tediko <tediko123@wp.pl>
2021-06-18 12:09:18 +0200

    Add: compile Sass in webpack setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,9 @@ let mix = require('laravel-mix');
 // Compile modern JavaScript & Sass
 mix.js('src/js/index.js', 'js')
     .react()
+    .sass('src/sass/main.scss', 'css').options({
+        processCssUrls: false
+    })
     .setPublicPath('dist');
 
 // Disable success notifications
