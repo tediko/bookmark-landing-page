8fdbf7fa1c87243895d0031a0192d3c7ac558792
tediko <tediko123@wp.pl>
2021-06-21 15:37:28 +0200

    Remove: key from li element

--- src/js/components/FaqAccordionItem.js
+++ src/js/components/FaqAccordionItem.js
@@ -1,6 +1,6 @@
-const FaqAccordionItem = ({ faq, index}) => {
+const FaqAccordionItem = ({ faq }) => {
     return ( 
-        <li className="faq__accordion-item" key={index} onClick={() => open(index)}>
+        <li className="faq__accordion-item">
             <h4 className="faq__accordion-question heading--xm">{faq.question}</h4>
             <div className="faq__accordion-toggle">
                 <p className="faq__accordion-desc paragraph--l">{faq.answer}</p>
