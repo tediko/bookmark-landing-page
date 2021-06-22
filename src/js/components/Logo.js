bc2b2ab35e5567afeff17b390d988faae34a7b8e
tediko <tediko123@wp.pl>
2021-06-22 17:30:49 +0200

    Add: Logo component

---
+++ src/js/components/Logo.js
@@ -0,0 +1,12 @@
+import blackLogo from '../../assets/img/logo-bookmark.svg';
+import whiteLogo from '../../assets/img/logo-bookmark-white.svg';
+
+const Logo = ({ className, color, altText }) => {
+    return ( 
+        <a href="#" className={`${className}__logo`}>
+            <img src={color === 'white' ? whiteLogo : blackLogo} alt={altText} className={`${className}__logo-img`} />
+        </a>
+     );
+}
+ 
+export default Logo;
