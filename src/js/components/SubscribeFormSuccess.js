f585c00b70c1e45a73e5509c330bbce8a7fc8e90
tediko <tediko123@wp.pl>
2021-06-22 16:21:11 +0200

    Add: SubscribeFormSuccess component

---
+++ src/js/components/SubscribeFormSuccess.js
@@ -0,0 +1,7 @@
+const SubscribeFormSuccess = () => {
+    return (
+        <h1 className="subscribe__form-success heading--l">Welcome! Thanks for subscribing!</h1>
+    );
+}
+ 
+export default SubscribeFormSuccess;
