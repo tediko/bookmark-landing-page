92938a24474a8e4271e49e5f6d4ef2a49301d073
tediko <tediko123@wp.pl>
2021-06-22 16:22:12 +0200

    Change: Imported useFormValidation

--- src/js/components/SubscribeForm.js
+++ src/js/components/SubscribeForm.js
@@ -1,11 +1,15 @@
 import Button from "./Button";
+import useFormValidation from "./useFormValidation";
 
-const SubscribeForm = () => {
+const SubscribeForm = ({ submitForm }) => {
+    const { handleChange, handleSubmit, mail, error, errorMessage } = useFormValidation(submitForm);
+
     return ( 
-        <form action="#" className="subscribe__form">
+        <form action="#" noValidate className={`subscribe__form ${error ? 'error' : ''}`} onSubmit={handleSubmit}>
             <label htmlFor="email-sub" className="subscribe__form-label sr-only">Subscribe to newsletter!</label>
-            <input type="email" name="email" id="email-sub" className="subscribe__form-input" placeholder="Enter your email address" />
+            <input type="email" name="email" id="email-sub" className="subscribe__form-input" placeholder="Enter your email address" value={mail} onChange={handleChange} />
             <Button className="subscribe__form-cta" name="contact" color="red" text="Contact Us" />
+            {error && <span className="subscribe__form-error">{errorMessage}</span>}
         </form>
      );
 }
