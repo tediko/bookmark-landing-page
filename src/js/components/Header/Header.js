8c413eca56cc7e7d5227c5bd43b812dedcef9830
tediko <tediko123@wp.pl>
2021-06-24 12:12:34 +0200

    Add: Imported useToggleMenu() and add span depending on isOpen value

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
+import useToggleMenu from './useToggleMenu';
 import MobileNav from './MobileNav';
 import DesktopNav from './DesktopNav';
 import Logo from '../shared/Logo';
 
 const Header = () => {
+    const { handleToggle, restoreToDefault, isOpen, isClosing, isExpanded, tabletBreakpoint } = useToggleMenu();
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     useEffect(() => {
         window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+        windowWidth >= tabletBreakpoint ? restoreToDefault() : '';
     })
     
     return ( 
         <header className="header">
             <div className="header__container container">
-                <Logo className="header" color="black" altText="Bookmark - Home page" />
-                {windowWidth > 768 ? <DesktopNav /> : <MobileNav />}
+                <Logo className="header" color={`${isOpen ? 'white' : 'black'}`} altText="Bookmark - Home page" />
+                {windowWidth >= tabletBreakpoint ? <DesktopNav /> : <MobileNav handleToggle={handleToggle} isOpen={isOpen} isClosing={isClosing} isExpanded={isExpanded} />}
+                {isOpen ? <span className={`header__menu-overlay ${isClosing ? 'hide' : ''}`}></span> : null}
             </div>
         </header>
      );
