b5809b77d772e2ecd708cf449aa27bee64ae2bee
tediko <tediko123@wp.pl>
2021-06-24 12:13:42 +0200

    Add: classNames depending on properties passed to component

--- src/js/components/Header/MobileNav.js
+++ src/js/components/Header/MobileNav.js
@@ -1,13 +1,13 @@
 import Icon from '../shared/Icon';
 import Button from '../shared/Button';
 
-const MobileNav = () => {
+const MobileNav = ({ handleToggle, isOpen, isClosing, isExpanded }) => {
     return ( 
         <nav className="header__nav mobile-nav">
-            <button className="mobile-nav__menu" aria-label="Menu toggle" aria-expanded="false" aria-controls="nav-list">
+            <button className={`mobile-nav__menu ${isOpen ? 'is-active' : ''}`} aria-label="Menu toggle" aria-expanded={isExpanded} aria-controls="nav-list" onClick={handleToggle}>
                 <span className="mobile-nav__menu-bars"></span>
             </button>
-            <div className="mobile-nav__wrapper">
+            <div className={`mobile-nav__wrapper ${isOpen ? 'is-active' : ''} ${isClosing ? 'is-closing' : ''}`}>
                 <ul className="mobile-nav__list" id="nav-list">
                     <li className="mobile-nav__item">
                         <a href="#" className="mobile-nav__link">Features</a>
