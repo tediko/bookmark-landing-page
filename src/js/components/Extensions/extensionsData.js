dff7e0eac16ba9640efda95e7104e7ff8e7ba65a
tediko <tediko123@wp.pl>
2021-06-27 14:03:05 +0200

    Add: File with data for extensions

---
+++ src/js/components/Extensions/extensionsData.js
@@ -0,0 +1,24 @@
+import chromeLogo from '../../../assets/img/logo-chrome.svg';
+import firefoxLogo from '../../../assets/img/logo-firefox.svg';
+import operaLogo from '../../../assets/img/logo-opera.svg';
+
+export const extensionsData = [
+    {
+        id: 1,
+        logo: chromeLogo,
+        title: 'Chrome',
+        version: '62'
+    },
+    {
+        id: 2,
+        logo: firefoxLogo,
+        title: 'Firefox',
+        version: '55'
+    },
+    {
+        id: 3,
+        logo: operaLogo,
+        title: 'Opera',
+        version: '46'
+    }
+]
