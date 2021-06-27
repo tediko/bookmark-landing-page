d83d7fad6f743be3320e5431b934da1be38f3358
tediko <tediko123@wp.pl>
2021-06-27 13:24:27 +0200

    Change: Instead of text props I add children props to access whatever is put between the component opening and closing tags

--- src/js/components/shared/Button.js
+++ src/js/components/shared/Button.js
@@ -1,8 +1,8 @@
-const Button = ({ type, className, name, color, text, label }) => {
+const Button = ({ children, type, className, name, color, text, label }) => {
     return ( 
         type === 'anchor' ? 
-        <a href="#" className={`${className} btn--${name} btn--${color}`}>{text}</a> :
-        <button className={`${className} btn--${name} btn--${color}`} aria-label={label ? label : null}>{text}</button>
+        <a href="#" className={`${className} btn--${name} btn--${color}`}>{children}</a> :
+        <button className={`${className} btn--${name} btn--${color}`} aria-label={label ? label : null}>{children}</button>
      );
 }
  
