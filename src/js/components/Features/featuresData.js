52930a9610bc5bf18eb115abb5c2a7141f1dc6f6
tediko <tediko123@wp.pl>
2021-06-27 14:16:15 +0200

    Add: File with features data

---
+++ src/js/components/Features/featuresData.js
@@ -0,0 +1,47 @@
+export const tabsInfoData = [
+    {
+        id: 0,
+        title: 'Bookmark in one click',
+        text: `Organize your bookmarks however you like. Our simple drag-and-drop interface 
+        gives you complete control over how you manage your favourite sites.`,
+        img: `url(/images/illustration-features-tab-1.svg)`,
+        imgSize: '89.4%'
+    },
+    {
+        id: 1,
+        title: 'Intelligent search',
+        text: `Our powerful search feature will help you find saved sites in no time at all. 
+        No need to trawl through all of your bookmarks.`,
+        img: `url(/images/illustration-features-tab-2.svg)`,
+        imgSize: '77%'
+    },
+    {
+        id: 2,
+        title: 'Share your bookmarks',
+        text: `Easily share your bookmarks and collections with others. Create a shareable 
+        link that you can send at the click of a button.`,
+        img: `url(/images/illustration-features-tab-3.svg)`,
+        imgSize: '77%'
+    }
+]
+
+export const tabsData = [
+    {
+        index: 0,
+        idName: 'bookmarking',
+        isChecked: true,
+        labelText: 'Simple Bookmarking'
+    },
+    {
+        index: 1,
+        idName: 'searching',
+        isChecked: false,
+        labelText: 'Speedy Searching'
+    },
+    {
+        index: 2,
+        idName: 'sharing',
+        isChecked: false,
+        labelText: 'Easy Sharing'
+    }
+]
